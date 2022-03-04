cd0cc3fff2cf8b95a8870b3e734515d15ae5adf8
ttyS3 <ttys3.rust@gmail.com>
2022-03-04 12:31:43 +0800

    feat: add ctrl+a>z for zoom toggle

---
+++ zoom_toggle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module documentation.
+"""
+
+def main(args):
+    pass
+
+from kittens.tui.handler import result_handler
+@result_handler(no_ui=True)
+def handle_result(args, answer, target_window_id, boss):
+    tab = boss.active_tab
+    if tab is not None:
+        if tab.current_layout.name == 'stack':
+            tab.last_used_layout()
+        else:
+            tab.goto_layout('stack')
