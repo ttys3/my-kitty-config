2a48a549c8d8cdcc2c61e1227b3db811e9d30679
ttyS3 <ttys3.rust@gmail.com>
2022-03-11 00:11:48 +0800

    refactor: add custom-hints.py
    Refs: https://sw.kovidgoyal.net/kitty/kittens/hints/#completely-customizing-the-matching-and-actions-of-the-kitten

---
+++ custom-hints.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+kitty -o 'map f1 kitten hints --customize-processing custom-hints.py'
+When you press the F1 key you will be able to select a word to look it up in the Google dictionary.
+https://sw.kovidgoyal.net/kitty/kittens/hints/#completely-customizing-the-matching-and-actions-of-the-kitten
+"""
+
+import re
+
+def mark(text, args, Mark, extra_cli_args, *a):
+    # This function is responsible for finding all
+    # matching text. extra_cli_args are any extra arguments
+    # passed on the command line when invoking the kitten.
+    # We mark all individual word for potential selection
+    for idx, m in enumerate(re.finditer(r'\w+', text)):
+        start, end = m.span()
+        mark_text = text[start:end].replace('\n', '').replace('\0', '')
+        # The empty dictionary below will be available as groupdicts
+        # in handle_result() and can contain arbitrary data.
+        yield Mark(idx, start, end, mark_text, {})
+
+
+def handle_result(args, data, target_window_id, boss, extra_cli_args, *a):
+    # This function is responsible for performing some
+    # action on the selected text.
+    # matches is a list of the selected entries and groupdicts contains
+    # the arbitrary data associated with each entry in mark() above
+    matches, groupdicts = [], []
+    for m, g in zip(data['match'], data['groupdicts']):
+        if m:
+            matches.append(m), groupdicts.append(g)
+    for word, match_data in zip(matches, groupdicts):
+        # Lookup the word in a dictionary, the open_url function
+        # will open the provided url in the system browser
+        boss.open_url(f'https://www.google.com/search?q=define:{word}')
