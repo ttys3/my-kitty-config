28a257bbb6db6deeb12d26e0690a4ef1cbf15ecf
ttyS3 <ttys3.rust@gmail.com>
2022-04-13 00:56:56 +0800

    refactor: make ctrl+a>z more like tmux prefix z zoom operation

--- zoom_toggle.py
+++ zoom_toggle.py
@@ -11,7 +11,9 @@ from kittens.tui.handler import result_handler
 def handle_result(args, answer, target_window_id, boss):
     tab = boss.active_tab
     if tab is not None:
-        if tab.current_layout.name == 'stack':
-            tab.last_used_layout()
-        else:
-            tab.goto_layout('stack')
+        # do not switch to stack layout if we have only one window
+        if len(tab.windows) > 1:
+            if tab.current_layout.name == 'stack':
+                tab.last_used_layout()
+            else:
+                tab.goto_layout('stack')
