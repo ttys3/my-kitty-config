06afeffb0f8ed75c8aa4f4b29628aaf7fcbe27e3
ttyS3 <ttys3.rust@gmail.com>
2024-09-26 12:54:53 +0000

    fix: fix zoom sometimes not working issue

--- zoom_toggle.py
+++ zoom_toggle.py
@@ -15,6 +15,6 @@ def handle_result(args, answer, target_window_id, boss):
         # do not switch to stack layout if we have only one window
         if len(tab.windows) > 1:
             if tab.current_layout.name == 'stack':
-                tab.last_used_layout()
+                tab.goto_layout('splits')
             else:
                 tab.goto_layout('stack')
